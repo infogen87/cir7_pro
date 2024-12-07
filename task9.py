1d60842e10d9a373855130c115031543b7f3419e
uzevie George <uzeviegeorge@gmail.com>
2024-12-07 09:49:50 +0100

    new task

---
+++ task9.py
@@ -0,0 +1,37 @@
+def findMedianSortedArrays(nums1, nums2):
+    
+    if len(nums1) > len(nums2):
+        nums1, nums2 = nums2, nums1
+    
+    x, y = len(nums1), len(nums2)
+    low, high = 0, x
+
+    while low <= high:
+        partitionX = (low + high) // 2
+        partitionY = (x + y + 1) // 2 - partitionX
+
+
+        maxX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
+        minX = float('inf') if partitionX == x else nums1[partitionX]
+
+        maxY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+        minY = float('inf') if partitionY == y else nums2[partitionY]
+
+
+        if maxX <= minY and maxY <= minX:
+            
+            if (x + y) % 2 == 1:
+                return max(maxX, maxY)
+            
+            return (max(maxX, maxY) + min(minX, minY)) / 2
+        
+        
+        elif maxX > minY:
+            high = partitionX - 1  
+        else:
+            low = partitionX + 1  
+
+
+num1 = [1, 2, 3]
+num2 = [5, 7, 8, 10]
+print(findMedianSortedArrays(num1, num2))
